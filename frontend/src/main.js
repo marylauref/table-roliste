0e0b0612a42a45b5f08130ccfbcec96247f4856c
Marylaure <marylaure.faverjon@gmail.com>
2024-05-25 22:59:43 +0200

    add libraries

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,31 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import "vuetify/styles"
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "fa",
+        aliases,
+        sets: {
+            fa,
+        },
+    },
+});
+
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
